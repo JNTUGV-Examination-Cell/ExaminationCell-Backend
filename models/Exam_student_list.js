76b6ed5c06ab2a7e97ceb306a01d88d365becfe7
khyathiyadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2023-10-07 11:39:52 +0000

    Added Examination,Exam_student_list models

---
+++ models/Exam_student_list.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const Examination = require('./Examination')
+const Students = require('./Student')
+
+
+
+const Exam_students_list = sequelize.define('exam_students_list', {
+    exam_code:{
+        type:DataTypes.STRING,
+        references:{
+            model: Examination,
+            key: 'exam_code'
+        },
+        allowNull: false
+    },
+    student_id:{
+        type:DataTypes.STRING,
+        references:{
+            model:Students ,
+            key: 'student_id'
+        },
+        allowNull: false
+
+    },
+    id:{
+        type:DataTypes.INTEGER,
+        primaryKey:true
+    },
+    qualified_status:{
+        type:DataTypes.STRING
+    }
+
+    
+
+    },{
+
+        tableName: 'Exam_students_list',    
+        timestamps: false  
+    }
+
+
+);
+
