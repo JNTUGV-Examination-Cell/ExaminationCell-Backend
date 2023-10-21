d66d6a79e7834b405fc56bd697ab886286e57e35
Ikaris-31 <malladilokesh2002@gmail.com>
2023-10-21 22:13:24 +0530

    test

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use('/api/staff', staffRoutes);
 app.use('/api/batch', batchRoutes);
 
 
-const models = [College, Staff,Attendence,Course, Branch, Regulation, Regulation_Courses, Batch, User, Student,Otp, District, Regulation_Courses_Set, Subject,examination,Ipaddress,Notification,examination_students_list];
+const models = [College, Staff,Course, Branch,Regulation, Regulation_Courses, Batch, User,Attendence, Student,Otp, District, Regulation_Courses_Set, Subject,examination,Ipaddress,Notification,examination_students_list];
 Promise.all(models.map(model => model.sync()))
   .then(() => {
     console.log("All models synced");
